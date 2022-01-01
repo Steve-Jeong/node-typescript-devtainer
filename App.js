625aa6206473628df57af1aca11960429e55f1e3
Steve Jeong <jst0930@gmail.com>
2022-01-01 10:40:09 +0000

    first issue

---
+++ App.js
@@ -0,0 +1,9 @@
+"use strict";
+let message = 'Hello world';
+console.log(message);
+console.log('Another text');
+console.log('Hello TypeScript');
+console.log('Hello Steve Jeong');
+console.log('This is after tsc --watch');
+console.log('This is another line of F5');
+//# sourceMappingURL=App.js.map
---
+++ App.ts
@@ -0,0 +1,8 @@
+let message:string = 'Hello world';
+
+console.log(message);
+console.log('Another text');
+console.log('Hello TypeScript');
+console.log('Hello Steve Jeong');
+console.log('This is after tsc --watch');
+console.log('This is another line of F5');
